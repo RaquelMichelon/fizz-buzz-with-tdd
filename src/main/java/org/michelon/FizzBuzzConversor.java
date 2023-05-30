5c6ed916f47306faea5412b548210b84d385fd4b
RaquelMichelon <raquelmichelonaulas@gmail.com>
2023-05-30 11:59:19 +0100

    4th test green

--- src/main/java/org/michelon/FizzBuzzConversor.java
+++ src/main/java/org/michelon/FizzBuzzConversor.java
@@ -10,6 +10,9 @@ public class FizzBuzzConversor {
 
 
     public String convert(int number) {
+        if(number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
         if(number % 3 == 0) {
             return "Fizz";
         }
