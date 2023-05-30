b4258d724868afe78a77844cf0590f078470609b
RaquelMichelon <raquelmichelonaulas@gmail.com>
2023-05-30 11:28:08 +0100

    create red step TDD

--- src/main/java/org/michelon/FizzBuzzConversor.java
+++ src/main/java/org/michelon/FizzBuzzConversor.java
@@ -1,10 +1,17 @@
 package org.michelon;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @autor raqueldarellimichelon
  * created on 30/05/23 inside the package - org.michelon
  **/
 public class FizzBuzzConversor {
+
+
+    public String convert(int number) {
+        return "";
+    }
 }
 
 
--- src/test/java/org/michelon/FizzBuzzConversorTest.java
+++ src/test/java/org/michelon/FizzBuzzConversorTest.java
@@ -1,5 +1,7 @@
 package org.michelon;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -7,5 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
  * created on 30/05/23 inside the package - org.michelon
  **/
 class FizzBuzzConversorTest {
+    @Test
+    public void convertShouldReturnsItselfWhenNotMultipleOf3and5() {
+        FizzBuzzConversor fizzBuzzConversor = new FizzBuzzConversor();
+
+        String result = fizzBuzzConversor.convert(1);
+
+        assertEquals("1", result);
+    }
 
 }
